30d3901e22f5c65eb6ebf91ed4bc3000ef11628e
Yufei Li <liyufei21@mails.tsinghua.edu.cn>
2023-03-29 21:20:07 +0800

    feat: login page using antd

---
+++ src/pages/login.tsx
@@ -0,0 +1,54 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input, Layout, Space, Typography } from 'antd';
+
+const LoginScreen = () => {
+  const { Title } = Typography;
+
+  return (
+    <Layout>
+      <Layout style={{ width: "50%" }}>
+        <Title level={2}>Login</Title>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Please input your Username!' }]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your Password!' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              Forgot password?
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+            Or <a href="">Register now!</a>
+          </Form.Item>
+        </Form>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default LoginScreen;
