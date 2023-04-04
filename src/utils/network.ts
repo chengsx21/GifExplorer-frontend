485d673005973a28dad4c9d8f2af208ceb08c5cd
Yufei Li <liyufei21@mails.tsinghua.edu.cn>
2023-04-04 11:06:38 +0800

    refactor: update network communication with backend

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,51 +1,46 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 
-const network = axios.create({
-    baseURL: "",
+// Create a new axios instance
+const service = axios.create({
+    baseURL: "/api/", // API base_url
+    timeout: 10000,   // Request timeout
 });
 
-enum NetworkErrorType {
-    CORRUPTED_RESPONSE,
-    UNKNOWN_ERROR,
+// Set request interceptor
+service.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = token;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    },
+);
+
+// Error interface
+interface NetworkError {
+    code: number;
+    info: string;
+    data: any;
 }
 
-export class NetworkError extends Error {
-    type: NetworkErrorType;
-    message: string;
-
-    constructor(
-        _type: NetworkErrorType,
-        _message: string,
-    ) {
-        super();
-
-        this.type = _type;
-        this.message = _message;
-    }
-
-    toString(): string { return this.message; }
-    valueOf(): Object { return this.message; }
-}
-
+// Make general request
 export const request = async (
     url: string,
     method: "GET" | "POST" | "PUT" | "DELETE",
     data?: any,
 ) => {
-    const response = await network.request({ method, url, data })
-        .catch((err: AxiosError) => {
-            throw new NetworkError(
-                NetworkErrorType.UNKNOWN_ERROR,
-                `[${err.response?.status}] ` + (err.response?.data as any).info,
-            );
+    return await service.request({ method, url, data })
+        .then((response: AxiosResponse) => {
+            return response.data;
+        })
+        .catch((error: AxiosError<NetworkError>) => {
+            return Promise.reject({
+                status: error.response?.status,
+                ...error.response?.data,
+            });
         });
-
-    if (response?.data.code === 0) {
-        return { ...response.data, code: undefined };
-    } else {
-        throw new NetworkError(
-            NetworkErrorType.UNKNOWN_ERROR,
-            response?.data.info,
-        );
-    }
 };
