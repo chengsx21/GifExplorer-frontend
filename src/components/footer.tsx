9785e304c16df617e01410bff50dc7295a184530
Patrick Li <nine-point-eight-p@outlook.com>
2023-04-12 01:03:41 +0800

    feat: update footer display

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,27 @@
-import { Layout } from 'antd';
+import { Layout, Divider } from 'antd';
 
 const { Footer } = Layout;
 
 const footerStyle: React.CSSProperties = {
-    textAlign: 'center',
-    color: '#fff',
-    backgroundColor: '#6495ED',
+    color: 'rgba(255, 255, 255, 0.65)',
+    backgroundColor: '#001529',
+    padding: 0,
+    height: '48px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
 };
 
 // Footer of the index page
 export const MainFooter = () => {
     return (
         <Footer style={footerStyle}>
-            GIF Explorer ©2023 Created by Nullptr
+            GIF Explorer
+            <Divider
+                type="vertical"
+                style={{ height: '40%', borderInlineStart: '1px solid rgba(255, 255, 255, 0.65)', translate: '0 2px'}}
+            />
+            © 2023 Nullptr
         </Footer>
     );
 };
